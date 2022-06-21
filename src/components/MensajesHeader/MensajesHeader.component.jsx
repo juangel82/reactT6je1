e9fabc9a5727b27b73e9cd49f6d679c8b43e2a85
juangel82 <juanillo82@gmail.com>
2022-06-21 09:48:05 +0200

    validacion

--- src/components/MensajesHeader/MensajesHeader.component.jsx
+++ src/components/MensajesHeader/MensajesHeader.component.jsx
@@ -1,6 +1,31 @@
 import './MensajesHeader.component.css';
 import { Formik,Form } from 'formik';
+import * as Yup from 'yup';
 export default function MensajesHeader(props) {
+
+
+
+
+  const validaciones = Yup.object().shape({
+    asunto: Yup.string()
+      
+      .required('Por favor, escribe el asunto.'),
+        
+    email: Yup.string()
+    .email('formato invalido')
+      .required('Por favor, escribe el email.'),
+  
+    mensaje: Yup.string()
+      .required("Por favor, escribe el mensaje")
+      .min(10,'Mínimo 10 caracteres'),
+      
+  });
+
+  const errors ={
+    asunto: 'asunto obligatorio.',
+    email: 'formato incorrecto|email obligatorio',
+    mensaje: 'mensaje obligatorio' | '10 caracteres mínimo'
+  }
 
   let initialValues = { asunto: '', email: '', mensaje: '' };
 
@@ -12,16 +37,19 @@ const manejarEnvio  = (e)=>{
   return (
     <div className="MensajesHeader">
 
-<Formik initialValues={initialValues} onSubmit={manejarEnvio}>
+<Formik initialValues={initialValues} onSubmit={manejarEnvio} validationSchema={validaciones}>
     {({ values, handleChange, handleSubmit, isSubmitting,errors}) => (
     <Form onSubmit={handleSubmit} className="Formulario">
       
       <input className="Elemento" placeholder="Introduce el asunto..." type="text"
              name="asunto" onChange={handleChange} value={values.asunto} />
+             {errors.asunto ? (<div>{errors.asunto}</div>) : null}
    <input className="Elemento" placeholder="Introduce tu mail" type="text"
              name="email" onChange={handleChange} value={values.email} />
+             {errors.email ? (<div>{errors.email}</div>) : null}
       <input className="Elemento" placeholder="Introduce el mensaje" type="text"
              name="mensaje" onChange={handleChange} value={values.mensaje} />
+             {errors.mensaje ? (<div>{errors.mensaje}</div>) : null}
   
      
   
